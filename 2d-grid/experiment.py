edf84f70e11762c1de24c3dcea9fdc1224026b69
Ruohan Zhang <zharu@utexas.edu>
2015-10-23 17:30:36 -0500

    starting to write python sim

---
+++ 2d-grid/experiment.py
@@ -0,0 +1,14 @@
+"""
+2D grid world simulation of traffic control
+Experiment file
+"""
+
+import world
+import vehicle
+
+class Experiment:
+    def __init__(self, vis = False):
+        pass
+
+    def run(self):
+        pass
