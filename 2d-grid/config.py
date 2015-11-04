9cc76500de0fe3fa89d254fbf5ea28dbd32a34dc
Jayen <jayen@cse.unsw.edu.au>
2015-11-03 23:41:55 -0600

    nothing much

--- 2d-grid/config.py
+++ 2d-grid/config.py
@@ -11,13 +11,13 @@ INTERSECT = 0
 ROAD = 1
 OFFROAD = 2
 # number of road blocks between 2 intersections
-NUM_BLK_BTW = 2 
+NUM_BLK_BTW = 3 
 # The default car density
 DEF_TRAFFIC = 0
 # Map
 CAP_MAX = 999
-CAP_HZ_ROAD = [1, 20, 30, 20, 10]
-CAP_VT_ROAD = [1, 30, 30, 20, 10]
+CAP_HZ_ROAD = [20, 30, 20, 10]
+CAP_VT_ROAD = [30, 30, 20, 10]
 # CONGEST_FACTOR = 1 if we want congest_prob to be 1.0 when current traffic is 200% capacity
 # decrease this value to make penalty more harsh for violating upper bound 
 CONGEST_FACTOR = 0.5 
@@ -29,6 +29,6 @@ ACTIONS = [UP, DOWN, LEFT, RIGHT, STAY]
 NUM_CARS = 20
 
 # for visualization
-CELL_SIZE = 40
+CELL_SIZE = 50
 
 
